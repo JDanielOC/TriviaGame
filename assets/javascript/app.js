f86b604ef03fec5ccbe0c182eed6442f5e33e9d6
Jason <vivanaranja@gmail.com>
2017-10-21 13:17:30 -0700

    update html for buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // build a library of questions with true false answers 
 // five questions
-// time limit of 30 seconds -- game ends at 30 secons and reports number of correct/incorrect answers
+// time limit of 30 seconds -- game ends at 30 seconds and reports number of correct/incorrect answers
 // include a countdown timer with a start timer (quiz) button.
 
 correctAnswers = 0;
@@ -9,7 +9,7 @@ incorrectAnswers = 0;
 $(document).ready(function() {
 
 
-    $("#startButton").click(function() {
+    $("#startButton").click(function(event) {
 
         $("#startButton").prop("disabled", "true");
         $("#startButton").fadeTo( "fast", 0.50 );
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
         })();
 
-    })
+    });
 
 
 
